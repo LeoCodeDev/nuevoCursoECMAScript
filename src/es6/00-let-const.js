ac85d926d1d49a0c7b2da422996f45b288e29231
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-09-29 17:05:07 -0500

    Primer Comit

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,26 @@
+var lastname = 'David';
+lastname = 'Oscar';
+console.log(lastname);
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+console.log(fruit);
+
+const animal = 'Dog';
+animal = 'Cat';
+console.log(animal);
+
+const fruits = () => {
+    if (true){
+        var fruit1 = 'Apple';
+        let fruit2 = 'Kiwi';
+        const fruit3 = 'Banana';
+
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruits();
+
---
+++ src/es6/01-arrowFunction.js
@@ -0,0 +1,9 @@
+function square(num) {
+    return num * num;
+}
+
+square(9);
+
+const square = (num) => {
+    return num * num;
+}
