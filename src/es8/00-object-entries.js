96928ff585eb6d87e3a6267ac411f3c41c2ed76f
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-09-30 17:44:57 -0500

    Final clase 16, ES8 object entries, object values y object keys.

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,11 @@
+const countries = {
+    MX: 'Mexico',
+    CO: 'Colombia',
+    VE: 'Venezuela',
+    PE: 'Peru',
+    CL: 'Chile'
+}
+
+console.log(Object.keys(countries));
+console.log(Object.values(countries));
+console.log(Object.entries(countries));
