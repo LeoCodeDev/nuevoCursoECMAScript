cfc841e31960e139eb1aca3a43e6e6ba98dcecc2
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-09-30 18:52:46 -0500

    Final clase 17, string padding y Trailing comas

---
+++ src/es8/01-string-padding.js
@@ -0,0 +1,4 @@
+const hello = 'Hello';
+
+console.log(hello.padStart(10, 'x'));
+console.log(hello.padEnd(10, 'Z'));
---
+++ src/es8/02-trailings-commas.js
@@ -0,0 +1,5 @@
+const age = [24, 32, 56, 18, 29, 33, 34];
+
+console.log(age);
+console.log(age.length);
+
