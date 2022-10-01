773600b2c92b8ceb816e0df64620a0671eaee488
Leopoldo Martinez <alb.mar.cta@gmail.com>
2022-09-30 19:10:43 -0500

    Fianlizada clase 18, funciones asincronas (async y await)

---
+++ src/es8/03-async-function.js
@@ -0,0 +1,17 @@
+const fxAsync = () => {
+    return new Promise((resolve, reject)=>{
+        (true)
+        ? setTimeout(() => resolve('Async!!'), 2000)
+        : reject(new Error('Error!'));
+    })
+}
+
+const anotherFx = async () => {
+    const something = await fxAsync();
+    console.log(something);
+    console.log('Hello');
+}
+
+console.log('before');
+anotherFx();
+console.log('after');
